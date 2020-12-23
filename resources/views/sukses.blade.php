f5096faec8c8b38d537d773f3c922a68df7df4c6
Ma'mur Mulyawan <mamurmulya@gmail.com>
2020-12-23 12:51:00 +0700

    Create sukses.blade.php

---
+++ resources/views/sukses.blade.php
@@ -0,0 +1,22 @@
+@extends('template.main')
+
+@section('konten')
+			<!-- section 1 -->
+			<div class="container my-5">
+				<div class="row">
+					<div class="col-md-3"></div>
+					<div class="col-md-6">
+						<div class="card border-success">
+							<div class="card-header border-success bg-sukses">Pendaftaran Berhasil</div>
+							<div class="card-body">
+								<p>Selamat Anda telah mendaftar sebagai peserta untuk mengikuti pelaksanaan uji kompetensi.</p>
+								<br>
+								<a href="/masuk" class="btn btn-primary">Masuk</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3"></div>
+				</div>
+			</div>
+			<!-- section temp -->
+@endsection
